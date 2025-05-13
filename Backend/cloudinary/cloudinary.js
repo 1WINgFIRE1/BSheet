97001e330c029d8602d11f9fdaf4532c2c205613
satyam gupta <143722450+1WINgFIRE1@users.noreply.github.com>
2025-05-13 17:43:29 +0530

    Update cloudinary.js

--- Backend/cloudinary/cloudinary.js
+++ Backend/cloudinary/cloudinary.js
@@ -7,9 +7,4 @@ dotenv.config();
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
 });
-console.log("Cloudinary Config:");
-console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
-console.log("API_KEY:", process.env.API_KEY);
-console.log("API_SECRET:", process.env.API_SECRET);
-
 export default cloudinary;
