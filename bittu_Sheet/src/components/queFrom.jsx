806c46d2c879f656ac23a6e7c488502ad731e489
satyam gupta <143722450+1WINgFIRE1@users.noreply.github.com>
2025-05-13 18:12:31 +0530

    Update queFrom.jsx

--- bittu_Sheet/src/components/queFrom.jsx
+++ bittu_Sheet/src/components/queFrom.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import styles from "./queForm.module.css"
 import axios from "axios"
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { ThemeContext } from '../context/themeContext.jsx';
 export default function QueFrom() {
     const[que, setQue] = useState("");
     const[ans, setAns] = useState("");
     const [loading, setLoading] = useState(false)
     const [toast, setToast] = useState(null)
+    const queFileInput = useRef(null)
+    const ansFileInput = useRef(null)
+    const formRef = useRef(null)
     const questionSubmit = async(e)=>{
         e.preventDefault();
         const formData = new FormData(e.target)
@@ -16,12 +19,15 @@ export default function QueFrom() {
             {headers: { "Content-Type": "multipart/form-data" }
         });
         setLoading(false)
+        queFileInput.current.value = null
+        ansFileInput.current.value = null
         setToast(res.data)
         setTimeout(() => {
             setToast(null)
         }, 3000);
         setQue("");
-        setAns("");       
+        setAns(""); 
+        formRef.current.reset()  
     }
     // fetching the old theme from local storage
     const theme = useContext(ThemeContext)
@@ -39,7 +45,7 @@ export default function QueFrom() {
                 </div>: null
             }
             <h1 className={`${styles.title} ${themeMode === "dark" ? styles.whiteFont: null}`}>Question Form</h1>
-            <form onSubmit={questionSubmit} className={`${styles.form} ${themeMode === "dark" ? styles.darkForm : styles.lightForm}`}>
+            <form ref={formRef} onSubmit={questionSubmit} className={`${styles.form} ${themeMode === "dark" ? styles.darkForm : styles.lightForm}`}>
                 <div className={styles.formGroup}>
                     <label htmlFor="Question" className={`${styles.label} ${themeMode === "dark" ? styles.whiteFont: null}`}>Question</label>
                     <textarea 
@@ -56,6 +62,7 @@ export default function QueFrom() {
                 <div className={styles.formGroup}>
                     <label htmlFor="QuestionImg" className={`${styles.label} ${themeMode === "dark" ? styles.whiteFont: null}`}>Uplaod your question (one image per qustion)*</label>
                     <input 
+                    ref={queFileInput}
                     type="file" 
                     id="QuestionImg" 
                     name="questionImg" 
@@ -80,6 +87,7 @@ export default function QueFrom() {
                 <div className={styles.formGroup}>
                     <label htmlFor="QuestionImg" className={`${styles.label} ${themeMode === "dark" ? styles.whiteFont: null}`}>Uplaod your answer (one image per answer)*</label>
                     <input 
+                    ref={ansFileInput}
                     type="file" 
                     id="AnswerImg" 
                     name="answerImg" 
